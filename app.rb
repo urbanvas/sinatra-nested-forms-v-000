ba3524c4a2f8d0dcfa8f815b6f32dab32109e98d
Neil Seejoor <nvseejoor@gmail.com>
2018-05-14 02:27:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,9 +5,10 @@ module FormsLab
 
     # code other routes/actions here
     get '/' do
-
+      "Welcome to the Nested Forms Lab!"
+      "let's navigate to the '/new'"
     end
-    
+
     get '/new' do
       erb :'pirates/new'
     end
