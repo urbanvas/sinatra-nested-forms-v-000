799677d1007f45d6a98b6e4a2c334e94c09a6fb7
Neil Seejoor <nvseejoor@gmail.com>
2018-05-14 02:09:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ module FormsLab
     end
 
     post '/pirates' do
-      # @pirate = Pirate.new(params[pirate][name], params[pirate][weight], params[pirate][height])
+      @pirate = Pirate.new(params['pirate']['name'], params['pirate']['weight'], params['pirate']['height'])
       erb :'pirates/show'
     end
 
